ac2ed06be70ce322f4a75490dddf02a710920be1
Robert Mausolf <rmausolf06@gmail.com>
2017-12-07 20:11:12 +0000

    update url

--- app.js
+++ app.js
@@ -16,8 +16,11 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-// mongoose.connect("mongodb://localhost/yelp_camp_v12", {useMongoClient: true});
-mongoose.connect("mongodb://Bob:Test@ds133856.mlab.com:33856/yelpcamp", {useMongoClient: true});
+   
+mongoose.connect(process.env.DATABASEURL, {useMongoClient: true});
+// mongoose.connect("mongodb://Bob:Test@ds133856.mlab.com:33856/yelpcamp", {useMongoClient: true});
+
+
 mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
